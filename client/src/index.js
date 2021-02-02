ad69021fc5469ca1ba711ec7a841c1549f6d2ce5
Michael <michael52423@gmail.com>
2021-02-02 16:24:25 +1100

    Refactored Sass with variables + mixins

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./scss/index.scss";
 import App from "./components/App/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
